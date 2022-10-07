4c4a5edcfb972d712d472309bdbc7e31cbff8672
Alejandro Mejía <alejandromejia2013.27@gmail.com>
2022-10-06 21:04:08 -0300

    Adding module template

---
+++ libs/dht11/inc/dht11.h
@@ -0,0 +1,16 @@
+/* DHT11
+ * Copyright YYYY Author Compelte Name <author@mail.com>
+ * All rights reserved.
+ * License: license text or at least name and link 
+         (example: BSD-3-Clause <https://opensource.org/licenses/BSD-3-Clause>)
+ *
+ * Version: 0.0.0
+ * Creation Date: YYYY/MM/DD
+ */
+
+/*=====[Avoid multiple inclusion - begin]====================================*/
+
+#ifndef _DHT_11_H_
+#define _DHT_11_H_
+
+#endif /* _DHT_11_H_ */
---
+++ libs/dht11/src/dht11.c
@@ -0,0 +1,10 @@
+/* DHT11 Module
+ * Copyright YYYY Author Complete Name <author@mail.com>
+ * All rights reserved.
+ * License: license text or at least name and link 
+         (example: BSD-3-Clause <https://opensource.org/licenses/BSD-3-Clause>)
+ *
+ * Version: 0.0.0
+ * Creation Date: YYYY/MM/DD
+ */
+#include "dht11.h"
