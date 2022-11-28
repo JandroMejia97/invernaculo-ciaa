d106958d86556eeb2706cdd98b921931710d06e9
Alejandro Mejía <alejandromejia2013.27@gmail.com>
2022-11-28 10:15:11 -0300

    Adding initial code for DHTxx libs

--- libs/dht11/inc/dht11.h
+++ libs/dht11/inc/dht11.h
@@ -9,8 +9,49 @@
  */
 
 /*=====[Avoid multiple inclusion - begin]====================================*/
+#include "sapi_datatypes.h"
 
 #ifndef _DHT_11_H_
 #define _DHT_11_H_
+
+#define DHTxx_PIN (GPIO01)
+#define DHTxx_SENSOR_TYPE
+#define DHTxx_SENSOR_TYPE_DHT11 (1)
+#define DHTxx_SENSOR_TYPE_DHT22 (2)
+
+#if DHTxx_SENSOR_TYPE_DHT11
+    #define DHTxx_SENSOR_PERIOD_MS (1000)
+#elif DHTxx_SENSOR_TYPE_DHT22
+    #define DHTxx_SENSOR_PERIOD_MS (2000)
+#else
+    #error "DHTxx_SENSOR_TYPE not defined"
+#endif
+
+typedef enum {
+    // OK
+    DHTxx_OK,
+    // No pull-up resistor
+    DHTxx_NO_PULLUP, 
+    // No 0 from acknowledge detected
+    DHTxx_NO_ACK_0,
+    // No 1 from acknowledge detected
+    DHTxx_NO_ACK_1,
+    // No LOW level detected
+    DHTxx_NO_DATA_0,
+    // No HIGH level detected
+    DHTxx_NO_DATA_1,
+    // Checksum error
+    DHTxx_ERROR_CHECKSUM,
+} DHTxx_Status_t;
+
+typedef struct {
+    DHTxx_Status_t status;
+    uint8_t humidity;
+    uint8_t temperature;
+} DHTxx_Data_t;
+
+const unsigned char *DHTxx_StatusToString(DHTxx_Status_t status);
+
+DHTxx_Status_t DHTxx_ReadData(uint16_t *temperature, uint16_t *humidity);
 
 #endif /* _DHT_11_H_ */
--- libs/dht11/src/dht11.c
+++ libs/dht11/src/dht11.c
@@ -8,3 +8,35 @@
  * Creation Date: YYYY/MM/DD
  */
 #include "dht11.h"
+
+const unsigned char *DHTxx_StatusToString(DHTxx_Status_t status) {
+    switch(status) {
+        case DHTxx_OK:
+            return "OK";
+        case DHTxx_NO_PULLUP:
+            return "NO_PULLUP";
+        case DHTxx_NO_ACK_0:
+            return "NO_ACK_0";
+        case DHTxx_NO_ACK_1:
+            return "NO_ACK_1";
+        case DHTxx_NO_DATA_0:
+            return "NO_DATA_0";
+        case DHTxx_NO_DATA_1:
+            return "NO_DATA_1";
+        case DHTxx_ERROR_CHECKSUM:
+            return "ERROR_CHECKSUM";
+        default:
+            return "UNKNOWN";
+    }
+}
+
+DHTxx_Status_t DHTxx_ReadData(uint16_t *temperature, uint16_t *humidity) {
+    uint8_t counter = 0, loopBits = 40, i = 0;
+    uint8_t buffer[5];
+    uint16_t data;
+
+    // Initialize buffer
+    for (i = 0; i < sizeof(buffer); i++) {
+        buffer[i] = 0;
+    }
+}
