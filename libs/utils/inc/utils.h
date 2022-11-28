9c33be0946a8102e6e6016f1e8a172fe10744e1b
Alejandro Mejía <alejandromejia2013.27@gmail.com>
2022-11-27 21:40:02 -0300

    Adding a utils library

---
+++ libs/utils/inc/utils.h
@@ -0,0 +1,16 @@
+/* Utils
+ * Copyright YYYY Author Compelte Name <author@mail.com>
+ * All rights reserved.
+ * License: license text or at least name and link 
+         (example: BSD-3-Clause <https://opensource.org/licenses/BSD-3-Clause>)
+ *
+ * Version: 0.0.0
+ * Creation Date: YYYY/MM/DD
+ */
+
+/*=====[Avoid multiple inclusion - begin]====================================*/
+
+#ifndef _UTILS_H_
+#define _UTILS_H_
+char* itoa(int value, char* result, int base);
+#endif /* _UTILS_H_ */
